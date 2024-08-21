5a803319176e3b1a32cf7c37c962407dc177caba
Trafalgar-D-ACID <smnafizulislam@gmail.com>
2024-08-22 00:50:16 +0600

    final

--- oop.php
+++ oop.php
@@ -62,9 +62,6 @@ $book2 = new Book("To Kill a Mockingbird", 3);
 $member1 = new Member("John Doe");
 $member2 = new Member("Jane Smith");
 
-$member1->borrowBook($book1);
-$member2->borrowBook($book2);
-
 echo "Available Copies of '" . $book1->getTitle() . "': " . $book1->getAvailableCopies() . "\n";
 echo "Available Copies of '" . $book2->getTitle() . "': " . $book2->getAvailableCopies() . "\n";
 
